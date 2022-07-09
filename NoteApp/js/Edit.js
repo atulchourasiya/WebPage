25362a3fdce866462ae5855abde997f8ef456095
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2022-07-09 22:01:34 +0530

    Final commit
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

--- NoteApp/js/Edit.js
+++ NoteApp/js/Edit.js
@@ -1,67 +1,72 @@
 let closebtnreplace = document.getElementsByClassName("closebtnreplace");
-closebtnreplace[0].addEventListener('click',()=>{
-  document.getElementById('replacemenu').style.display ='none'
+let id;
+closebtnreplace[0].addEventListener('click', () => {
+   document.getElementById('replacemenu').style.display = 'none'
 });
 //Handle Copy
-function Copy(id){
- let button = document.getElementById('dropdownMenuButton1');   
- let text = document.getElementById(id);
- text.select();
- navigator.clipboard.writeText(text.value);
- button.innerHTML = `Copied<i class="fa-solid fa-paste ps-1"></i>`
- setTimeout(() => {
-    button.innerHTML = `Copy<i class="fa-solid fa-paste ps-1"></i>`
- }, 1000);
+function Copy(id) {
+   let button = document.getElementById('dropdownMenuButton1');
+   let text = document.getElementById(id);
+   navigator.clipboard.writeText(text.value);
+   button.innerHTML = `Copied<i class="fa-solid fa-paste ps-1"></i>`
+   setTimeout(() => {
+      button.innerHTML = `Copy<i class="fa-solid fa-paste ps-1"></i>`
+   }, 1000);
 }
 //Handle Uppercase
-function UpperCase(id){
- let text = document.getElementById(id);
- text.value = text.value.toUpperCase();
+function UpperCase(id) {
+   let text = document.getElementById(id);
+   text.value = text.value.toUpperCase();
 }
 //Handle Capitalize
-function capitalize(id){
- let text = document.getElementById(id);
- let TextValue = '';
- let firstletter = true;
- for (let word of text.value) {
-    if(word === ' ' || word === '\n')
-    {
-      TextValue += word;
-      firstletter = true;
-    }
-    else if(firstletter)
-    {
-       TextValue += word.toUpperCase();
-       firstletter = false;
-    }
-    else
-    {
-       TextValue += word.toLowerCase();
-    }
- }
- text.value = TextValue;
+function capitalize(id) {
+   let text = document.getElementById(id);
+   let TextValue = '';
+   let firstletter = true;
+   for (let word of text.value) {
+      if (word === ' ' || word === '\n') {
+         TextValue += word;
+         firstletter = true;
+      } else if (firstletter) {
+         TextValue += word.toUpperCase();
+         firstletter = false;
+      } else {
+         TextValue += word.toLowerCase();
+      }
+   }
+   text.value = TextValue;
 }
 //Handle Lowercase
-function LowerCase(id){
- let text = document.getElementById(id);
- text.value = text.value.toLowerCase();
+function LowerCase(id) {
+   let text = document.getElementById(id);
+   text.value = text.value.toLowerCase();
 }
 //Handle Reset
-function Reset(id){
- let text = document.getElementById(id);
- text.value = '';
+function Reset(id) {
+   let text = document.getElementById(id);
+   text.value = '';
 }
 //Handle Format
-function Format(id){
- let text = document.getElementById(id);
- let textValue = text.value.split(/[\n]+/);
- textValue = textValue.join("\n");
- textValue = textValue.split(/[ ]+/)
- textValue = textValue.join(" ");
- text.value = textValue;
+function Format(id) {
+   let text = document.getElementById(id);
+   let textValue = text.value.split(/[\n]+/);
+   textValue = textValue.join("\n");
+   textValue = textValue.split(/[ ]+/)
+   textValue = textValue.join(" ");
+   text.value = textValue;
 }
 //Handle Replace
-function Replace(id){
-  replacemenu.style.display = 'block';
- let text = document.getElementById(id);
+function Replace(identity) {
+   replacemenu.style.display = 'block';
+   id = document.getElementById(identity);
 }
+
+let replacebtn = document.getElementById('replacebtn');
+replacebtn.addEventListener('click', () => {
+   let replaceOldWord = document.getElementById('replaceOldWord');
+   let replaceNewWord = document.getElementById('replaceNewWord');
+   id.value = id.value.replaceAll(replaceOldWord.value, replaceNewWord.value);
+   replaceNewWord.value = ''
+   replaceOldWord.value = ''
+   replacemenu.style.display = 'none';
+});
